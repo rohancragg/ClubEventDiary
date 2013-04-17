ecb39adaf63d1cbeee8cc932c3f2a8595dbc0de7
Rohan Cragg <rohancragg@gmail.com>
2013-04-17 14:32:09 +0100

    Initial commit of src. Excluded bin and packages.

---
+++ ClubWebDiary/Models/ClubDiaryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ClubWebDiary.Models
+{
+    public class ClubDiaryDbContext : DbContext
+    {
+        public ClubDiaryDbContext()
+            : base(nameOrConnectionString: "ClubDiary") {}
+
+
+    }
+}
---
+++ ClubWebDiary/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClubWebDiary.Models
+{
+    public class Event
+    {
+        public DateTime EventDate { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
