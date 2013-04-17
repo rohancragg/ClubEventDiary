65f86b646a6dbae5464ba5aaebb936ec9469efd0
Rohan Cragg <rohancragg@gmail.com>
2013-04-17 18:36:29 +0100

    Added untracked files

---
+++ ClubEventDiary.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+using ClubWebDiary.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClubEventDiary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var db = new ClubDiaryDbContext())
+            {
+                Console.WriteLine(db.Events.Count());
+                db.Events.Add(new Event { Title = "Test 1", EventDate = DateTime.Now });
+                db.SaveChanges();
+                Console.WriteLine(db.Events.Count());
+            }
+            
+        }
+    }
+}
