3fd973bb227d1e425dba8e4ad8f1bb0d37c9de50
Rohan Cragg <rohancragg@gmail.com>
2013-04-24 09:12:57 +0100

    Hard-coded observable array (events) now working.

--- ClubWebDiary.Web/App/viewmodels/home.js
+++ ClubWebDiary.Web/App/viewmodels/home.js
@@ -1,25 +1,25 @@
-﻿define(['services/logger'], function (logger) {
-    var events = ko.observableArray();
+﻿define(['services/logger'],
+    function (logger) {
+        var events = ko.observableArray();
 
-    var vm = {
-        activate: activate,
-        events: events,
-        title: 'Events'
-    };
-    return vm;
+        var vm = {
+            activate: activate,
+            events: events,
+            title: 'Events'
+        };
 
-    return vm;
+        return vm;
 
-    //#region Internal Methods
-    function activate() {
-        // temp hard-coded data
-        events = [
-            { Id: 1, Title: 'Test1', Description: 'Test1 Desc', EventDate: '2013/01/01' },
-            { Id: 1, Title: 'Test2', Description: 'Test2 Desc', EventDate: '2013/08/01' },
-            { Id: 1, Title: 'Test3', Description: 'Test3 Desc', EventDate: '2013/15/01' }];
+        //#region Internal Methods
+        function activate() {
 
-        logger.log('Home View Activated', null, 'home', true);
-        return true;
-    }
-    //#endregion
-});
+            // temp hard-coded data
+            events([{ Id: 1, Title: 'Test1', Description: 'Test1 Desc', EventDate: '2013/01/01' },
+                { Id: 1, Title: 'Test2', Description: 'Test2 Desc', EventDate: '2013/08/01' },
+                { Id: 1, Title: 'Test3', Description: 'Test3 Desc', EventDate: '2013/15/01' }]);
+
+            logger.log('Home View Activated', null, 'home', true);
+            return true;
+        }
+        //#endregion
+    });
